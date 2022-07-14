e9ef2f0973d3db5e80f032f53941126d36337145
shivajikarad <shivajikarad1414@gmail.com>
2022-07-14 22:06:44 +0530

    first time

---
+++ demo/src/demo/Demo.java
@@ -0,0 +1,16 @@
+package demo;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=20;
+		int c=a+b;
+		
+		System.out.println(c);
+		System.out.println(a-b);	
+		
+	}
+	
+}
